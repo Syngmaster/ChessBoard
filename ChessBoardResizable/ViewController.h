4843aa2334abfcdd6587e1ebd7470cb7fa263363
Syngmaster <max6361@mail.ru>
2017-04-18 19:03:35 +0100

    Initial Commit

---
+++ ChessBoardResizable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChessBoardResizable
+//
+//  Created by Syngmaster on 18/04/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
